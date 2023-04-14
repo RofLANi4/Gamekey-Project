ae6650f431224f4c83616ed1a1fd97dda3c4e087
Kevin Korduner <kevinfdkord@gmail.com>
2023-04-14 11:59:36 +0300

    function based views -> class based views

--- gamekeyservice/views.py
+++ gamekeyservice/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from models import Game
 
 
-def index(request):
-    return render(request, "index.html")
+class HomeView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Home"
+        context["hot_games"] = Game.objects.filter(price__discount__gt=0)
+
+        return context
