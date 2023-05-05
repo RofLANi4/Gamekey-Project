d2e125c311019285e86076eb83260b9a4a695b3b
Kevin Korduner <kevinfdkord@gmail.com>
2023-05-05 14:49:06 +0300

    change price from decimal to integer

--- gamekeyservice/models.py
+++ gamekeyservice/models.py
@@ -10,8 +10,8 @@ class Customer(AbstractUser):
 
 class Game(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2, default=0)
-    discount = models.DecimalField(max_digits=2, decimal_places=0, default=0)
+    price = models.IntegerField()
+    discount = models.IntegerField()
     description = models.CharField(max_length=255)
     image = models.ImageField(upload_to="images/")
     genre = models.ManyToManyField("Genre", related_name="games")
