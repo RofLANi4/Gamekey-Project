4bce5b171cad81c4986c60756c332e54f5633fc1
Kevin Korduner <kevinfdkord@gmail.com>
2023-05-09 16:27:09 +0300

    discounts and dates

---
+++ gamekeyservice/templatetags/discount_counter.py
@@ -0,0 +1,10 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter(name="discount")
+def discount_counter(price, discount):
+    if discount is None:
+        return price
+    return round(price - price * (discount / 100), 2)
