73c472af7f86a42b25b08d12733f7ec2706d3490
RofLANi4 <artemclassics19@gmail.com>
2023-05-13 15:57:58 +0300

    add game list, in search

--- static/js/header.js
+++ static/js/header.js
@@ -1,4 +1,52 @@
 const search = document.querySelector(".search"),
-  input = document.getElementById("mySearch");
+  input = document.getElementById("mySearch"),
+  searchRquestActive = document.querySelector(".search-request"),
+  inputActive = document.querySelector(".input");
 
 search.onclick = () => (input.value = ""); //Очищает поисковую строку
+
+function sendSearchRequest() {
+  let searchTerm = document.getElementById("mySearch").value;
+  let url = "/gamekey/search/?q=" + searchTerm;
+
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (jsonResponse) {
+      console.log(jsonResponse);
+      searchRquestActive.classList.add("search-request-active");
+      inputActive.classList.add("input-active");
+      searchRquestActive.innerHTML = "";
+      if (jsonResponse.results.length != 0) {
+        // console.log(jsonResponse.results[0].name);
+        // console.log(jsonResponse.results[0].id);
+        for (key in jsonResponse.results) {
+          console.log(jsonResponse.results[key]);
+          searchRquestActive.innerHTML += `<a href="/gamekey/game-page/${jsonResponse.results[key].id}">${jsonResponse.results[key].name}</a>`;
+        }
+        // searchRquestActive.innerHTML = `<a href="/gamekey/game-page/${jsonResponse.results[0].id}">${jsonResponse.results[0].name}</a>`;
+      } else if (jsonResponse.results.length == 0) {
+        searchRquestActive.innerHTML = "";
+        searchRquestActive.classList.remove("search-request-active");
+        inputActive.classList.remove("input-active");
+      }
+    });
+
+  // let xhr = new XMLHttpRequest();
+  // xhr.open("GET", url, true);
+
+  // xhr.responseType = "json";
+  // xhr.onload = function () {
+  //   if (xhr.readyState === 4 && xhr.status === 200) {
+  //     let results = xhr.response;
+  //     // console.log(results); // handle the results here
+  //   }
+  // };
+
+  // xhr.send();
+}
+
+document
+  .getElementById("mySearch")
+  .addEventListener("input", sendSearchRequest);
--- staticfiles/js/header.js
+++ staticfiles/js/header.js
@@ -1,4 +1,46 @@
 const search = document.querySelector(".search"),
-  input = document.getElementById("mySearch");
+  input = document.getElementById("mySearch"),
+  searchRquestActive = document.querySelector(".search-request"),
+  inputActive = document.querySelector(".input");
 
 search.onclick = () => (input.value = ""); //Очищает поисковую строку
+
+function sendSearchRequest() {
+  let searchTerm = document.getElementById("mySearch").value;
+  let url = "/gamekey/search/?q=" + searchTerm;
+
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (jsonResponse) {
+      console.log(jsonResponse);
+      if (jsonResponse.results.length != 0) {
+        console.log(jsonResponse.results[0].name);
+        console.log(jsonResponse.results[0].id);
+        searchRquestActive.innerHTML = `<a href="{% url 'game_keys:game_page' pk=game.${jsonResponse.results[0].id} %}"${jsonResponse.results[0].name}</a>`;
+      } else if (jsonResponse.results.length == 0) {
+        searchRquestActive.innerHTML = "";
+      }
+    });
+
+  // let xhr = new XMLHttpRequest();
+  // xhr.open("GET", url, true);
+
+  // xhr.responseType = "json";
+  // xhr.onload = function () {
+  //   if (xhr.readyState === 4 && xhr.status === 200) {
+  //     let results = xhr.response;
+  //     // console.log(results); // handle the results here
+  //   }
+  // };
+
+  // xhr.send();
+}
+
+document
+  .getElementById("mySearch")
+  .addEventListener("input", sendSearchRequest);
+
+searchRquestActive.classList.add("search-request-active");
+inputActive.classList.add("input-active");
